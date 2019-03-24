0fe893a06c276b4ba1b940bf2681e4d69b0b6fd1
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-03-24 20:38:32 +0900

    refactor: CI Settings (#9)
    * refactor: CI Settings
    
    * refactor: Remove unused settings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export const add = (a: number, b: number): number => a + b
+export function add(a: number, b: number): number {
+  return a + b;
+}
 
-export const sub = (a: number, b: number): number => a - b
+export function sub(a: number, b: number): number {
+  return a - b;
+}
